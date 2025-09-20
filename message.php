0ec355d73b6b82aa05b8b00f8130635af6c01f34
daprince11 <taiwooyindun@gmail.com>
2025-09-20 11:33:52 +0100

    Add files via upload

---
+++ message.php
@@ -0,0 +1,27 @@
+<?php
+  //let's get all the values
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $website = $_POST['website'];
+  $message = $_POST['message'];
+
+  if(!empty($email) && !empty($message)){ //if email and message field is not empty
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){ //if user entered email is valid
+      $receiver = "taiwooyindun@gmail.com"; //email receiver email address
+      $subject = "From: $name <$email>"; //subject of the email. Subject looks like From: Name <email>
+      //merging concating all user values inside body variable. \n is used for new line
+      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage: $message\n\nRegards,\n$name";
+      $sender = "From: $email"; //sender email
+      if(mail($receiver, $subject, $body, $sender)){ //mail() is a inbuilt php function to send mail
+        echo "Your message has been sent";
+      }else{
+        echo "Sorry, failed to send your message!";
+      }
+    }else{
+      echo "Enter a valid email address!";
+    }
+  }else{
+    echo "Email and message field is required!";
+  }
+?>
---
+++ script.js
@@ -0,0 +1,30 @@
+//let's get all required elements
+
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
+
+form.onsubmit = (e)=>{
+  e.preventDefault(); //preventing form from submitting
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+
+  let xhr = new XMLHttpRequest(); //creating new xml object
+  xhr.open("POST", "message.php", true); //sending message request to message.php file
+  xhr.onload = ()=>{//once ajax loaded
+    if(xhr.readyState == 4 && xhr.status == 200){ //if ajax response status is 200 & ready status is 4 means there is no error
+      let response = xhr.response; //storing ajax response in a response variable
+      //if response is an error like enter valid address then we'll change status color to red else reset the form
+      if(response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") || response.indexOf("Sorry, failed to send your message!")){
+        statusTxt.style.color = "red";
+      }else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000); //hide the statusTxt after 3 seconds if the msg is sent
+      }
+      statusTxt.innerText = response;
+    }
+  }
+  let formData = new FormData(form); //creating new formData obj. This obj is used to send form data
+  xhr.send(formData) //sending form data
+}
