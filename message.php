18e3aba4e09365f26078329fc449b3e5a1c99651
daprince11 <taiwooyindun@gmail.com>
2025-09-20 12:18:14 +0100

    nothing important

--- message.php
+++ message.php
@@ -1,10 +1,10 @@
 <?php
   //let's get all the values
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $phone = $_POST['phone'];
-  $website = $_POST['website'];
-  $message = $_POST['message'];
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $phone = htmlspecialchars($_POST['phone']);
+  $website = htmlspecialchars($_POST['website']);
+  $message = htmlspecialchars($_POST['message']);
 
   if(!empty($email) && !empty($message)){ //if email and message field is not empty
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){ //if user entered email is valid
--- script.js
+++ script.js
@@ -14,13 +14,13 @@ form.onsubmit = (e)=>{
     if(xhr.readyState == 4 && xhr.status == 200){ //if ajax response status is 200 & ready status is 4 means there is no error
       let response = xhr.response; //storing ajax response in a response variable
       //if response is an error like enter valid address then we'll change status color to red else reset the form
-      if(response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") || response.indexOf("Sorry, failed to send your message!")){
+      if(response.indexOf("Email and message field is required!") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message!") != -1){
         statusTxt.style.color = "red";
       }else{
         form.reset();
         setTimeout(()=>{
           statusTxt.style.display = "none";
-        }, 3); //hide the statusTxt after 3 seconds if the msg is sent
+        }, 3000); //hide the statusTxt after 3 seconds if the msg is sent
       }
       statusTxt.innerText = response;
     }
